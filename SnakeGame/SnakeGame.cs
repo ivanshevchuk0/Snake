d6b6cba67e28f6d8b8699e880e9377bdf1f23f61
Ivan Shevchuk <103109247+ivanshevchuk0@users.noreply.github.com>
2025-03-23 21:25:16 +0100

    Add SnakeGame class and initial game setup; remove empty Program.cs

---
+++ SnakeGame/SnakeGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SnakeGame
+{
+    class SnakeGame
+    {
+        static int width = 20;
+        static int height = 20;
+        static char[,] field = new char[width, height];
+        static List<(int x, int y)> snake = new List<(int x, int y)>();
+        static (int x, int y) apple;
+        static Random random = new Random();
+        static ConsoleKey direction = ConsoleKey.RightArrow;
+
+        static void Initialize()
+        {
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    field[i, j] = ' ';
+                }
+                snake.Add((width / 2, height / 2));
+                GenerateApple();
+            }
+        }
+
+        static void GenerateApple()
+        {
+            do
+            {
+                apple = (random.Next(0, width), random.Next(0, height));
+            }while(snake.Contains(apple));
+            field[apple.x, apple.y] = '*';
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
